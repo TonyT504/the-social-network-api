4b628a9ccfbb0cd2c5b30ad98952574f12dbcf5c
Turbo PC <tonytran504@gmail.com>
2022-07-09 17:33:43 -0500

    building models cont

--- models/thought.js
+++ models/thought.js
@@ -1,33 +1,65 @@
 const { Schema, model } = require('mongoose');
 // const assignmentSchema = require('./Assignment');
+const formatDate = require("../utils/date")
+const reactionSchema = new Schema({
 
-// Schema to create Student model
-const studentSchema = new Schema(
-  {
-    first: {
-      type: String,
-      required: true,
-      max_length: 50,
-    },
-    last: {
-      type: String,
-      required: true,
-      max_length: 50,
-    },
-    github: {
-      type: String,
-      required: true,
-      max_length: 50,
-    },
-    assignments: [assignmentSchema],
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId()
+
   },
+  reactionBody: {
+    type: String,
+    Required: true,
+    max_length: 280
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+    get: timestamp => formatDate(timestamp),
+  },
+
+
+
+},
   {
     toJSON: {
       getters: true,
     },
+  })
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      max_length: 280,
+      min_length: 1
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => formatDate(timestamp),
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true
+    },
   }
 );
+thoughtSchema.virtual("reactioncount").get(function () {
+  return this.reactions.length
+})
+const Thought = model('Thought', thoughtSchema);
 
-const Student = model('student', studentSchema);
-
-module.exports = Student;
+module.exports = Thought;
