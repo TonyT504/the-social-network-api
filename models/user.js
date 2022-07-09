7dac9ac632167680954d79806170ce35f3aefe21
Turbo PC <tonytran504@gmail.com>
2022-07-09 17:14:07 -0500

    building models

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a course model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+
+
+    },
+    email: {
+      type: string,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "invalid email address"]
+    },
+   
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+userSchema.virtual("friendcount").get(function(){
+  return this.friends.length 
+})
+const User = model('User', userSchema);
+
+module.exports = User;
